eb989ba40fd11b6a96a5eb3d4c98efa990eba017
acunacinema-ctrl <acunacinema@gmail.com>
2026-07-16 11:08:32 -0400

    Update FilmsSection.jsx

--- lumiere-shadow-frame-2/src/components/portfolio/FilmsSection.jsx
+++ lumiere-shadow-frame-2/src/components/portfolio/FilmsSection.jsx
@@ -6,7 +6,7 @@ const films = [
   {
     title: 'Rachel & Martin',
     location: 'Holland, Michigan',
-    year: '2026',
+    year: '2025',
     cover: 'https://media.base44.com/images/public/69af8d2c4b2e24d43b5fa37a/b54ec9c85_67a7b444-7ebf-4528-9b7f-64aafc9c949e.png',
     filmUrl: 'https://vimeo.com/1178909337?share=copy&fl=sv&fe=ci',
   },
